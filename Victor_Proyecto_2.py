cef4e498ef3ee29f66e5fe7936b5ebf660af8c8a
norbertogithub21092022 <norbertoarguellomartinez007@gmail.com>
2022-11-15 00:36:48 -0600

    Update Victor_Proyecto_2.py

--- Victor_Proyecto_2.py
+++ Victor_Proyecto_2.py
@@ -1,16 +1,13 @@
-from tkinter import Menu
-Menu = '''Escriba 1) para suma de n numeros,
+print('''Escriba 1) para suma de n numeros, 
       2) para producto de n numeros, 
       3) para division de 2 numeros, 
       4) para factorial, 
       5) para tablas, 
       6) para cuadrado y cubo, 
       7) para promedio, 
-      8) para valor maximo y minimo'''
-
-print(Menu)
+      8) para valor maximo y minimo''')
       
-operacion = input("1, 2, 3, 4, 5, 6, 7, 8 ");
+operacion = int(input("1, 2, 3, 4, 5, 6, 7, 8 "))
  
 if operacion == 1:
       print(' No hay operacion')
@@ -45,32 +42,32 @@ elif operacion == 5:
             while i <=40:
                   print(i)
                   i += 4
-      elif tabla == 2:
+      elif tabla == 5:
             i=5
             while i <=50:
                   print(i)
                   i += 5
-      elif tabla == 2:
+      elif tabla == 6:
             i=6
             while i <=60:
                   print(i)
                   i += 6
-      elif tabla == 2:
+      elif tabla == 7:
             i=7
             while i <=70:
                   print(i)
                   i += 7
-      elif tabla == 2:
+      elif tabla == 8:
             i=8
             while i <=80:
                   print(i)
                   i += 8
-      elif tabla == 2:
+      elif tabla == 9:
             i=9
             while i <=90:
                   print(i)
                   i += 9
-      elif tabla == 2:
+      elif tabla == 10:
             i=10
             while i <=100:
                   print(i)
